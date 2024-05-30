68046858bd56712a97e0df1768348a06be4cded1
JohannPouponnot <jpouponnot@acheel.com>
2024-05-30 19:48:06 +0200

    chore: some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,17 @@ export default function Home() {
   const { user } = useAuth()
 
   return (
-    <div>
+    <div
+      style={{
+        padding: '72px 12px 12px',
+      }}
+    >
       <SignedIn>
         <p>Welcome, {user?.name}</p>
         <LogoutButton>
           <button className="blue">Déconnexion</button>
         </LogoutButton>
       </SignedIn>
-
-      <SignedOut>
-        <p>Not authenticated</p>
-        <LoginButton>
-          <button>Connexion</button>
-        </LoginButton>
-      </SignedOut>
     </div>
   )
 }
